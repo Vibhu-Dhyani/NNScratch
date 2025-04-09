e65b53e83da09859ea5f431681dadfc7c221f080
Vibhu-Dhyani <vibhudhyani28@gmail.com>
2025-04-09 07:27:40 +0530

    building Rnn.forward and Rnn.backdrop

--- rNN/main.py
+++ rNN/main.py
@@ -1,20 +1,32 @@
+import numpy as np
 from createInputs import createInput
 from dataset import train_data
 from preProcesser import preProcessing
 from rnn import RNN
-from tabulate import tabulate
+
+
+def softmax(x):
+    return np.exp(x) / sum(np.exp(x))
+
 
 # Pre-Process and create a vocab and assing numbers to each word in vocab
 idx_to_w, w_to_idx = preProcessing()
-print(idx_to_w)
-inputs = []
-# Create Inputs for the train data
-for sentence in train_data.keys():
-    inputs.append(createInput(sentence, len(idx_to_w), w_to_idx))
 
-# for i in inputs:
-#    print(tabulate(i, tablefmt="fancy_grid"))
 
-# Create our rnn
+# Create our RNN
 ourRnn = RNN(2, len(idx_to_w))
-print(tabulate(ourRnn.Whh, tablefmt="fancy_grid"))
+
+
+# Create Inputs for the train data
+for x, y in train_data.items():
+    input = createInput(x, len(idx_to_w), w_to_idx)
+    target = int(y)
+
+    y, h = ourRnn.forward(input)
+    prob = softmax(y)
+
+    # dl_dy
+    dl_dy = prob
+    dl_dy[target] -= 1
+
+    ourRnn.backprop(dl_dy)
--- rNN/rnn.py
+++ rNN/rnn.py
@@ -9,5 +9,29 @@ class RNN:
         self.bh = np.zeros((hidden_size, 1))
         self.by = np.zeros((output_size, 1))
 
-    def forward(self):
-        pass
+    def forward(self, input):
+        h = np.zeros((64, 1))
+
+        # Caching for backprop
+        self.hMap = {0: h}
+        self.last_input = input
+
+        for i, a in enumerate(input):
+            h = np.tanh(self.Whh @ h + self.Wxh @ a + self.bh)
+            self.hMap[i] = h
+        y = self.Why @ h + self.by
+        return y, h
+
+    def backprop(self, dl_dy, learningRate = 2e-2):
+
+        # dl_dWhy
+        dl_dWhy = self.hMap[len(hMap)-1] * dl_dy
+
+        # dl_dby
+        dl_dby = dl_dy
+
+        # dl_dWhh = dl_dy * sum(dy_ht) * dht_dWhh 
+        # calc sum (dy_dht) = dy/dht+1 (1-ht^2)Whh
+        dl_dht = self.Why
+        for h in reversed(list(self.hMap.keys())) :
+            
